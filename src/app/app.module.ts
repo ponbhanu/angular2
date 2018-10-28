7b3d89b740257b8a2279f22467e5bac07e4b466f
ponbhanu <42290909+ponbhanu@users.noreply.github.com>
2018-10-29 00:15:01 +0530

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { MainComponent } from './main/main.component';
-import { FooterComponent } from './footer/footer.component';
 
+import { HttpModule } from '@angular/http';
 @NgModule({
   declarations: [
-    AppComponent,
-    HeaderComponent,
-    MainComponent,
-    FooterComponent
+    AppComponent
+    
   ],
   imports: [
-    BrowserModule
+    BrowserModule,HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
