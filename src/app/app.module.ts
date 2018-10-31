63e49cd71659c613850f3e5ac9dc23821367a857
ponbhanu <42290909+ponbhanu@users.noreply.github.com>
2018-10-31 10:26:25 +0530

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+//import {ToastModule} from 'ng2-toastr/ng2-toastr';
+import { HttpModule } from '@angular/http';
+//import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+    
+
 
 import { AppComponent } from './app.component';
 
-import { HttpModule } from '@angular/http';
+
 @NgModule({
   declarations: [
     AppComponent
-    
   ],
   imports: [
-    BrowserModule,HttpModule
+    BrowserModule,
+    //BrowserAnimationsModule,
+    HttpModule
+  //  ToastModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
