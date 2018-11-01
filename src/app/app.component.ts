0f67e21b4cf6a1bcb5bed4383716ca7916546e81
ponbhanu <42290909+ponbhanu@users.noreply.github.com>
2018-11-01 08:35:36 +0530

    Update app.component.ts
    ts changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,8 @@
 import { Component, ViewContainerRef } from '@angular/core';
-import { Http, Response, Headers , RequestOptions } from '@angular/http';
+import { Http, Response, Headers, RequestOptions } from '@angular/http';
 //import { ToastsManager } from 'ng2-toastr/ng2-toastr';
 import * as $ from 'jquery';
-
+import { ToasterService} from 'angular2-toaster';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -15,11 +15,11 @@ export class AppComponent {
   width: any;
   isTrue: any;
   id: any;
-  checkedFiles :any= [];
+  selectedFiles: any = [];
+  checkedFiles: any = [];
   isCheckBoxClicked: any = false;
-  constructor(public http: Http) {
-    
-
+  isRadioClicked: any = false;
+  constructor(public http: Http,public toasterService: ToasterService){
   }
 
   onChange(data) {
@@ -31,26 +31,20 @@ export class AppComponent {
     }
   }
 
-  uploadFiles(type: any, i: any) {
+  uploadFiles(type: any, i: any, selectedFiles) {
     this.id = i;
-    if (type === 'single') {
-      var file = this.files.filter((item) => item.id === this.id);
-      file = file[0].image
+    var file = '';
+    if (selectedFiles && selectedFiles.length && selectedFiles.length > 0) {
+      file = selectedFiles[this.id].image;
     } else {
-      var file = this.files[this.id].image;
+      this.toasterService.pop('error', 'Please select files to upload');
+      return;
     }
-    var elem = document.getElementById("bar"),
-      width = 1,
-      ids = setInterval(frame, 10);
-    function frame() {
-      if (width >= 100) {
-        clearInterval(i);
-      } else {
-        width++;
-        elem.style.width = width + '%';
-        elem.innerHTML = width * 1 + '%';
-      }
-    };
+    if (!this.isRadioClicked) {
+      this.toasterService.pop('error', 'Please select the primary file');
+      return;
+    }
+
     var headers = new Headers();
     for (var k = 0; k < this.files.length; k++) {
       if (this.files[k].isPrimary) {
@@ -58,50 +52,72 @@ export class AppComponent {
       }
     }
     let formData: FormData = new FormData();
-    formData.append('file', file);
+    formData.append('image', file);
     this.http.post('http://localhost:3000/api/uploadFile', formData, { headers: headers })
       .subscribe(response => {
-        if (response.json().resultCode === 'OK') {
-          if (type === 'single') {
-            for (var i = 0; i < this.files.length; i++) {
-              if (this.files[i].id === this.id) {
-                this.files[i].status = 'success';
-              }
-            }
-          } else {
-            this.files[this.id].status = 'success';
-          }
+        if (response.json().message === 'success') {
+          this.callLoader();
+          this.files[this.id].status = 'success';
           var id = this.id + 1;
-          if (type === 'multiple') {
-            if (id < this.files.length) {
-              this.uploadFiles('multiple', id);
+          if (selectedFiles && selectedFiles.length && selectedFiles.length > 0) {
+            if (id < selectedFiles.length) {
+              this.uploadFiles('multiple', id, this.selectedFiles);
             } else {
-      //        this.toastr.success('All files uploaded successfully');
+              this.toasterService.pop('success','All files uploaded successfully');
+              //this.toastr.success('All files uploaded successfully');
+            }
+          } else if (this.files && this.files.length && this.files.length > 0) {
+            if (id < this.files) {
+              this.uploadFiles('multiple', id, this.files);
+            } else {
+              this.toasterService.pop('success','All files uploaded successfully');
+            //  this.toastr.success('All files uploaded successfully');
             }
           }
         } else if (response.json().resultCode === 'KO') {
-          if (type === 'single') {
-            for (var i = 0; i < this.files.length; i++) {
-              if (this.files[i].id === this.id) {
-                this.files[i].status = 'fail';
-                
-              }
-            }
-          }
           $("#bar").removeClass("bar-grn");
           $("#bar").addClass("bar-red");
-            this.files[this.id].status = 'fail';
-        //    this.toastr.error('Files uploading has ben stopped');
+          this.files[this.id].status = 'fail';
+          
+              this.toasterService.pop('error', 'Files uploading has ben stopped');
+              //this.toastr.error('Files uploading has ben stopped');
         }
       });
   };
 
+
+  callLoader() {
+    var elem = document.getElementById("bar"),
+      width = 1;
+    var ids = setInterval(frame, 10);
+    function frame() {
+      if (width >= 100) {
+        clearInterval(ids);
+      } else {
+        width++;
+        elem.style.width = width + '%';
+        elem.innerHTML = width * 1 + '%';
+      }
+    };
+  }
+
+
   onClickRadio(data) {
+    this.isRadioClicked = true;
     for (var i = 0; i < this.files.length; i++) {
       if (this.files[i].id === data.id) {
         this.files[i].isPrimary = true;
       }
     }
+  }
+
+  onClickCheckBox(file) {
+    if (this.selectedFiles.indexOf(file) > -1) {
+      this.selectedFiles.splice(file, 1);
+    } else {
+      this.selectedFiles.push(file);
+    }
+    console.log(this.selectedFiles)
   }
 
   cancel() {
@@ -111,8 +127,6 @@ export class AppComponent {
   manageActions(id, type) {
     if (type === 'delete') {
       this.files = this.files.filter((item) => item.id !== id);
-    } else if (type === 'upload') {
-      this.uploadFiles('single', id);
     } else if (type === 'cancel') {
 
     }
@@ -120,7 +134,15 @@ export class AppComponent {
 
 
   uploadAllFiles() {
-    this.uploadFiles('multiple', 0);
+    this.uploadFiles('multiple', 0, this.selectedFiles);
   }
 }
+
+
+
+
+
+
+
+
 
